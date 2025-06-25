931eb163337e2ea6ab6e23490b10e3e4f141c8a6
Gustavo <gustavogomes1108@gmail.com>
2025-06-25 12:59:14 -0300

    .

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const botaoIniciar = document.querySelector("#btn-iniciar")
+const topLeft = document.querySelector(".top-left")
+const topRight = document.querySelector(".top-right")
+const bottomLeft = document.querySelector(".bottom-left")
+const bottomRight = document.querySelector(".bottom-right")
+
+botaoIniciar.addEventListener("click", ()=>{
+    //Aqui o jogo começa
+    
+    //Quero fazer uma animação antes do jogo começar mesmo
+
+
+
+
+})
