0626258ae7a31f65ebf4dd51d2e12db01a4ca72d
Gustavo <gustavogomes1108@gmail.com>
2025-06-25 22:54:40 -0300

    .

--- js/script.js
+++ js/script.js
@@ -4,12 +4,24 @@ const topRight = document.querySelector(".top-right")
 const bottomLeft = document.querySelector(".bottom-left")
 const bottomRight = document.querySelector(".bottom-right")
 
-botaoIniciar.addEventListener("click", ()=>{
-    //Aqui o jogo começa
-    
-    //Quero fazer uma animação antes do jogo começar mesmo
+botaoIniciar.addEventListener("click", () => {
+    topLeft.classList.add("ligado")
+    topRight.classList.add("ligado")
+    bottomLeft.classList.add("ligado")
+    bottomRight.classList.add("ligado")
 
+    setInterval(() => {
+        setTimeout(() => {
+            topLeft.classList.add("ligado")
+            topRight.classList.add("ligado")
+            bottomLeft.classList.add("ligado")
+            bottomRight.classList.add("ligado")
+        }, 200)
 
-
+        topLeft.classList.remove("ligado")
+        topRight.classList.remove("ligado")
+        bottomLeft.classList.remove("ligado")
+        bottomRight.classList.remove("ligado")
+    }, 400)
 
 })
